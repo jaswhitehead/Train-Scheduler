6762b8adbe904461d89e50909370fe887b775c69
Jason Whitehead <jaswhitehead@gmail.com>
2020-06-10 11:51:36 -0500

    Trying to add current time display to Jumbotron

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,51 +10,65 @@
     measurementId: "G-3ZRMCPKECW"
   };
 // Initialize Firebase
-  var train1 = firebase.initializeApp(firebaseConfig, 'train1');
+  firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
-// Initializez Database
+// Initializes Database
   var database = firebase.database();
 
-  // CAPTURE BUTTON CLICK
+//**Sets time for home page (NOT WORKING)
+//  $(document).ready(function () {
+//  setInterval(function () {
+//     $(".now").text(moment(now).format('h:mm:ss a'));
+//  }, 100);
+
+
+// CAPTURE BUTTON CLICK
     $(".submitButton").on("click",function(event) {
         event.preventDefault();
         console.log("Ready")
   
-  //VALUES FOR EACH VARIABLE IN HTML
-        var trainName = $("#trainName").val().trim();
-        var destination = $("#destination").val().trim();
-        var firstTrainTime = $("#firstTrainTime").val().trim();
-        var frequency = parseInt($("#frequency").val().trim());
+//VALUES FOR EACH VARIABLE IN HTML
+    var trainName = $("#trainName").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrainTime = $("#firstTrainTime").val().trim();
+    var frequency = parseInt($("#frequency").val().trim());
   
-        var train = {
-            trainName:trainName,
-            destination:destination,
-            firstTrainTime:firstTrainTime,
-            frequency:frequency
-  
-        };
-        database.ref().push(train)
-        $(".form-control").val("");
-  
-  })
+    var train = {
+      trainName:trainName,
+      destination:destination,
+      firstTrainTime:firstTrainTime,
+      frequency:frequency
+    };
+
+// Pushing to database
+      database.ref().push(train)
+      $(".form-control").val("");
+    })
 
 //ON CLICK CHILD FUNCTION
-      database.ref().on("child_added",function(childSnapshot){
+    database.ref().on("child_added",function(childSnapshot){
           
-          var trainData = childSnapshot.val()
-          var convertedFirstTrainTime = moment(trainData.firstTrainTime,"HH:mm");
-          var difference = moment().diff(moment(convertedFirstTrainTime),"minutes");
-          var timeRemaining = difference % trainData.frequency;db
-          var minutesAway = trainData.frequency - timeRemaining;
-          var nextArrival = moment().add(minutesAway,"minutes");
-          nextArrival = moment(nextArrival).format("HH:mm");
+      var trainData = childSnapshot.val()
+      // Difference between the current and firstTrain
+      var convertedFirstTrainTime = moment(trainData.firstTrainTime,"HH:mm");
+      var difference = moment().diff(moment(convertedFirstTrainTime),"minutes");
+      var timeRemaining = difference % trainData.frequency;db
+      // Minutes until next train
+      var minutesAway = trainData.frequency - timeRemaining;
+      // Next train time
+      var nextArrival = moment().add(minutesAway,"minutes");
+      nextArrival = moment(nextArrival).format("HH:mm");
 
-          console.log("Name: " + trainName);
-	        console.log("Destination: " + destination);
-        	console.log("Time: " + firstTrainTime);
-        	console.log("Frequency: " + frequency);
-
+      console.log("Name: " + trainName);
+	    console.log("Destination: " + destination);
+      console.log("Time: " + firstTrainTime);
+      console.log("Frequency: " + frequency);
+  
+// Handle the errors
+      }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+      });
   
 //APPEND TO DISPLAY IN TRAIN TABLE
         $("#trainInfo").append(
@@ -68,4 +82,5 @@
               </tr>
               `
           )
-        })
+         })
+    
